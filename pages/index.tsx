ed0dd114b2cae94e86fa32fec43a2e768cbf27ac
Eduardo Fujiwara <esfujiwara@topaz.com.br>
2021-07-11 13:17:00 -0300

    init commit

---
+++ pages/index.tsx
@@ -0,0 +1,42 @@
+import utilStyles from '../styles/utils.module.css'
+import Layout from '../components/layout'
+import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      {/* Keep the existing code here */}
+
+      {/* Add this <section> tag below the existing <section> tag */}
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  console.log(" >>>>>>>>>>>>>>>>>>")
+  console.log(allPostsData);
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
